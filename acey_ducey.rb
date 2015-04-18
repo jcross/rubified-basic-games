a0c2c00e407520b224a76333c35fccf1943e157e
James Andrew Cross <jms.cross@gmail.com>
2015-04-18 18:13:50 -0400

    Converted tabs to two spaces in acey_ducey.

--- acey_ducey.rb
+++ acey_ducey.rb
@@ -5,101 +5,101 @@
 # Ruby translation by James Cross (http://github.com/jcross/)
 
 module AceyDucey
-	CARD_FACE = { 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6", 7 => "7", 
-								8 => "8", 9 => "9", 10 => "10", 11 => "JACK", 12 => "QUEEN",
-								13 => "KING", 14 => "ACE" }
+  CARD_FACE = { 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6", 7 => "7",
+                8 => "8", 9 => "9", 10 => "10", 11 => "JACK", 12 => "QUEEN",
+                13 => "KING", 14 => "ACE" }
 
-	def self.cputs(text, term_width = 38)
-		# Centers text on the screen based on the optional term_width.
-		# 38 seems to be the terminal width targeted by these BASIC games.
-  	center = term_width - (text.length / 2)
-  	puts "#{' ' * center}#{text}"
-	end
+  def self.cputs(text, term_width = 38)
+    # Centers text on the screen based on the optional term_width.
+    # 38 seems to be the terminal width targeted by these BASIC games.
+    center = term_width - (text.length / 2)
+    puts "#{' ' * center}#{text}"
+  end
 
-	def self.display_intro
-		cputs "ACEY DUCEY CARD GAME"
-		cputs "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
-		cputs "TRANSLATED TO RUBY"
-		cputs "BY JAMES CROSS (http://github.com/jcross/)\n\n\n"
+  def self.display_intro
+    cputs "ACEY DUCEY CARD GAME"
+    cputs "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
+    cputs "TRANSLATED TO RUBY"
+    cputs "BY JAMES CROSS (http://github.com/jcross/)\n\n\n"
 
-		puts "ACEY-DUCEY IS PLAYED IN THE FOLLOWING MANNER"
-		puts "THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP"
-		puts "YOU HAVE AN OPTION TO BET OR NOT BET DEPENDING"
-		puts "ON WHETHER OR NOT YOU FEEL THE CARD WILL HAVE" # "nor" for "not" typo
-		puts "A VALUE BETWEEN THE FIRST TWO."
-		puts "IF YOU DO NOT WANT TO BET, INPUT A 0"
-		puts "TO QUIT, INPUT QUIT"
-	end
+    puts "ACEY-DUCEY IS PLAYED IN THE FOLLOWING MANNER"
+    puts "THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP"
+    puts "YOU HAVE AN OPTION TO BET OR NOT BET DEPENDING"
+    puts "ON WHETHER OR NOT YOU FEEL THE CARD WILL HAVE" # "nor" for "not" typo
+    puts "A VALUE BETWEEN THE FIRST TWO."
+    puts "IF YOU DO NOT WANT TO BET, INPUT A 0"
+    puts "TO QUIT, INPUT QUIT"
+  end
 
-	def self.generate_card
-		rand(2..14)
-	end
+  def self.generate_card
+    rand(2..14)
+  end
 
-	def self.generate_cards
-		first = 14
-		second = 0
-		until first < second
-			first = generate_card
-			second = generate_card
-		end
-		return first, second
-	end
+  def self.generate_cards
+    first = 14
+    second = 0
+    until first < second
+      first = generate_card
+      second = generate_card
+    end
+    return first, second
+  end
 
-	def self.main
-		money = 100
+  def self.main
+    money = 100
 
-		# This bit is duplicated below because we need to see this line at
-		# startup but not after a $0 bet resulting in "CHICKEN!!!"
-		puts "YOU NOW HAVE  #{money}  DOLLARS\n\n"
-		
-		while true
-			first_card, second_card = generate_cards
-			puts "HERE ARE YOUR NEXT TWO CARDS"
-			puts " #{CARD_FACE[first_card]}\n #{CARD_FACE[second_card]}\n\n"
-			print "WHAT IS YOUR BET? "
-			input = gets.chomp
-			break if input.upcase == "QUIT"
-			input = input.to_i
-			if input <= 0
-				puts "CHICKEN!!\n\n"
-			elsif input > money
-				puts "SORRY, MY FRIEND BUT YOU BET TOO MUCH"
-				puts "YOU HAVE ONLY  #{money}  DOLLARS TO BET\n\n"
-			else
-				third_card = generate_card
-				puts " #{CARD_FACE[third_card]}"
-				if third_card > first_card and third_card < second_card
-					puts "YOU WIN!!!"
-					money += input
-				else
-					puts "SORRY, YOU LOSE"
-					money -= input
-				end
-				
-				if money <= 0
-					puts "SORRY, FRIEND BUT YOU BLEW YOUR WAD"
-					print "TRY AGAIN (YES OR NO) "
-					input = gets.chomp.upcase
-					if input.start_with? "Y"
-						money = 100
-					else
-						break
-					end
-				end
+    # This bit is duplicated below because we need to see this line at
+    # startup but not after a $0 bet resulting in "CHICKEN!!!"
+    puts "YOU NOW HAVE  #{money}  DOLLARS\n\n"
+    
+    while true
+      first_card, second_card = generate_cards
+      puts "HERE ARE YOUR NEXT TWO CARDS"
+      puts " #{CARD_FACE[first_card]}\n #{CARD_FACE[second_card]}\n\n"
+      print "WHAT IS YOUR BET? "
+      input = gets.chomp
+      break if input.upcase == "QUIT"
+      input = input.to_i
+      if input <= 0
+        puts "CHICKEN!!\n\n"
+      elsif input > money
+        puts "SORRY, MY FRIEND BUT YOU BET TOO MUCH"
+        puts "YOU HAVE ONLY  #{money}  DOLLARS TO BET\n\n"
+      else
+        third_card = generate_card
+        puts " #{CARD_FACE[third_card]}"
+        if third_card > first_card and third_card < second_card
+          puts "YOU WIN!!!"
+          money += input
+        else
+          puts "SORRY, YOU LOSE"
+          money -= input
+        end
+        
+        if money <= 0
+          puts "SORRY, FRIEND BUT YOU BLEW YOUR WAD"
+          print "TRY AGAIN (YES OR NO) "
+          input = gets.chomp.upcase
+          if input.start_with? "Y"
+            money = 100
+          else
+            break
+          end
+        end
 
-				puts "YOU NOW HAVE  #{money}  DOLLARS\n\n"
-			end
-		end
-		
-		puts "OK HOPE YOU HAD FUN"
-	end
+        puts "YOU NOW HAVE  #{money}  DOLLARS\n\n"
+      end
+    end
+    
+    puts "OK HOPE YOU HAD FUN"
+  end
 
-	def self.run
-		display_intro
-		main
-	end
+  def self.run
+    display_intro
+    main
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-	AceyDucey.run
+  AceyDucey.run
 end
